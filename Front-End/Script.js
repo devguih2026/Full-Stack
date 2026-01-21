dae760ff2f02750465ea12f0b6b6019150116670
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-01-21 18:49:18 -0300

    Tela de cadastro para o CREATE do CRUD

---
+++ Front-End/Script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form")
+
+form.addEventListener("submit", function (event){
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+
+    fetch("http://127.0.0.1:5000/login", {
+        method: "POST",
+        headers: {
+             "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert("Usuário Logado com sucesso!");
+        console.log(data);
+    })
+    .catch(error => {
+    console.error("Erro:", error);
+    });
+});
